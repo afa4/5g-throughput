554a41c63fafc3bdfb08e72e6e884f8c7ded8455
Arthur Frade de Araujo <arthur.frade@loft.com.br>
2023-04-12 22:36:24 +0000

    throughput dynamic calculation

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # 5g throuput calculator
+
+### Example of execution
+
+```shell
+python main.py 1 27 100 4
+```
+
+### Expected result
+
+```shell
+1627
+```
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 import math
 
-bits_per_symbol_table = {
+BITS_PER_SYMBOL_TABLE = {
     0: 0.2344,
     1: 0.377,
     2: 0.6016,
@@ -31,30 +31,71 @@ bits_per_symbol_table = {
     26: 7.1602,
     27: 7.4063
 }
-
-slot_time_ms = 0.5
-slot_subcarier_frequency_khz = 30 
-slot_subcariers = 12
-bandwidth_mhz = 100 
-resource_blocks_guards = 4
-resource_blocks = math.floor((bandwidth_mhz * 1000) / (slot_subcariers * slot_subcarier_frequency_khz) - resource_blocks_guards)
-
-modulation_order = 27
-data_symbols = 11 # 14 - 2 for DRMS and 1 for PDCCH
-bits_per_symbol = bits_per_symbol_table.get(modulation_order)
-bits_per_slot = math.floor(slot_subcariers * data_symbols * bits_per_symbol)
-
-link_slots = 1600 # downlink or 1500 uplink 
-mimo_layers = 4
-throughput = math.floor(((bits_per_slot * resource_blocks * link_slots * mimo_layers) / 1024) / 1024)
+NUMEROLOGY_TABLE_KHZ = {
+    0: 15,
+    1: 30,
+    2: 60,
+    3: 120
+}
+SLOT_TIME_MS = 0.5
+RESOURCE_BLOCKS_GUARDS = 4
+SLOT_SUBCARIERS = 12
 
 
-def calc():
-    print(throughput)
+def throughput5g(input):
+    slot_subcarier_frequency_khz = NUMEROLOGY_TABLE_KHZ.get(
+        input.get('numerology'))
+    resource_blocks = math.floor((input.get('bandwidth') * 1000) / (
+        SLOT_SUBCARIERS * slot_subcarier_frequency_khz) - RESOURCE_BLOCKS_GUARDS)
+
+    modulation_order = 27
+    data_symbols = SLOT_SUBCARIERS - 3  # 2 slots for DRMS and 1 for PDCCH
+    bits_per_symbol = BITS_PER_SYMBOL_TABLE.get(modulation_order)
+    bits_per_slot = math.floor(
+        SLOT_SUBCARIERS * data_symbols * bits_per_symbol)
+
+    # 1600 downlink - 1500 uplink
+    link_slots = 1600 if input.get('link_type') == 0 else 1500
+
+    return math.floor(
+        ((bits_per_slot * resource_blocks * link_slots *
+         input.get('mimo_layers')) / 1024) / 1024
+    )
+
+
+def validate(input: dict):
+    if (input.get('numerology') < 0 or input.get('numerology') > 4):
+        raise Exception('invalid numerology, it should be between 1 and 4')
+    if (input.get('modulation_order') < 0 or input.get('modulation_order') > 27):
+        raise Exception(
+            'invalid modulation order, it should be between 1 and 27')
+    if (not [20, 50, 100, 400].__contains__(input.get('bandwidth'))):
+        raise Exception('invalid bandwidth, valid is 20, 50, 100 or 400 MHz')
+    if (not [1, 2, 4].__contains__(input.get('mimo_layers'))):
+        raise Exception('invalid mimo layers, valid are: 1, 2 or 4')
+
+
+def calculate_throughput(args) -> int:
+    if (len(args) < 4):
+        print('invalid number of arguments')
+        return
+    input = {
+        'numerology': args[0],
+        'modulation_order': args[1],
+        'bandwidth': args[2],
+        'mimo_layers': args[3],
+        'link_type': 0
+    }
+    if (len(args) == 5):
+        input['link_type'] = args[4]
+    validate(input)
+    return throughput5g(input)
 
 
 if __name__ == '__main__':
     args = sys.argv[1:]
-    for i, arg in enumerate(args):
-        print(f"Argument {i:>6}: {arg}")
-    calc()
+    try:
+        input_as_integers = list(map(lambda i: int(i),  args))
+        print(calculate_throughput(input_as_integers))
+    except Exception as e:
+        print('invalid arguments')
